a2c9854d340d79f9807153a91db76ccd370ae951
pashabratanov <pashaibratanov@gmail.com>
2022-07-27 21:41:36 +0300

    #task_2: done

--- task_2/truncate.rb
+++ task_2/truncate.rb
@@ -1,2 +1,11 @@
 module Truncate
+  def truncate(attr_name, length)
+    define_method attr_name do
+      required_length = length[:length]
+
+      str = instance_variable_get("@#{attr_name}")
+
+      required_length < str.length ? str.slice(0, required_length) + '...' : str
+    end
+  end
 end
